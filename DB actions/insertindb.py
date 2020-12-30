b9080f1c3902aa8fde419dd7c61acf5b8ec0b57f
Lucia Saura <nayade_2lu@hotmail.com>
2020-12-30 13:04:52 +0100

    clean-up

--- DB actions/insertindb.py
+++ DB actions/insertindb.py
@@ -2,7 +2,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="andorra1",
     database="datarepres2"
 )
 cursor = db.cursor()
